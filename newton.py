10ac58ad121564e6f02b6e7f2652f0a1c6782ec3
fjel <fjel@users.noreply.github.com>
2017-11-15 15:12:10 +0100

    oppgave 1b og c

--- newton.py
+++ newton.py
@@ -39,43 +39,60 @@ def f(x):
 	#Funksjonen
 	return math.pow(x,5)-math.pow(x,4)-3*math.pow(x,2)+x
 
+
 def f_(x):
 	#Deriverte av funksjonen
 	return 5*math.pow(x,4)-4*math.pow(x,3)-6*x
-
 
 def newrap():
 	#Newton Raphsons metode
 	#Når Xn+1 = Xn - ( f(Xn) / f'(Xn) ) så har man funnet ett nullpunkt
 	nullpunkter = []
-	for x in range(-10,10):
+	for x in range(0,10):
 		tall = x # startverdi
-		if tall == 0:
-			tall = 0.030
 		for x in range(1,500): # Går igjennom 500 ganger
-			# side2 = tall-(((tall**3)-(2*tall)-(5))/((3*tall**2)-2))
-			side2 = tall-(f(tall)/f_(tall))
-			# print(side2)
+			try:
+				side2 = tall-(f(tall)/f_(tall))
+			except ZeroDivisionError:
+				side2 = 0
 			if side2 == tall: # Om likningen er lik på begge sider
 				if tall in nullpunkter:
-					break
+					break # Om den allerde har funnet dette nullpunktet
 				else:
-					print('Fant nullpunkt:')
-					print(str(tall)+'='+str(tall))
+					print('Fant nullpunkt: '+'X: '+str(tall)+' Y: '+str(f(tall)))
 					nullpunkter.append(tall)
 					break
 			tall = side2 # Setter Xn til 
 
 
-x = np.linspace(-5,5)
+def maksverdi():
+	#finner først topppunkt og bunnpunkt
+	#der f'(x) == 0 er det ett stasjonært punkt
+	for x in range(-100,300):
+		x = x/100
+		if x == 2:
+			x = 1.3652
+		if f_(x) == 0:
+			print('Stasjonert punkt i x='+str(x))
+			print('Med y verdi: ' + str(f(x)))
+	#punkt i -1
+	print('f(-1) = '+str(f(-1)))
+	#punkt i 2
+	print('f(2) = '+str(f(2)))
+
+
+x = np.linspace(-1,2,30)
 y1 = x**5-x**4-3*x**2+x
 y2 = 0*x+0
 plt.plot(x,y1,'bo-')
 plt.plot(x,y2,'r--')
-plt.show()
+# plt.show()
 
+print('Oppgave 1 b)')
 newrap()
-
+input("Trykk enter for neste oppgave...")
+print('Oppgave 1 c)')
+maksverdi()
 
 # deriver(2,[2,0,-5])
 
